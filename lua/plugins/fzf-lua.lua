c743e67597b5ea13af3e9630d0bb0bb431c4e51d
Janne Vikstedt <vikstedt.janne@gmail.com>
2025-04-08 11:07:43 +0300

    Better configs for fzf-lua

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -3,18 +3,23 @@ return {
   keys = {
     {
       "<c-p>",
-      LazyVim.pick("files"),
+      LazyVim.pick("files", { root = false }),
       desc = "Find files",
     },
     {
       "<leader>ag",
-      LazyVim.pick("grep_cword"),
+      LazyVim.pick("grep_cword", { root = false }),
       desc = "Grep string",
     },
     {
       "<leader>p",
       "<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>",
       desc = "Search buffers",
+    },
+    {
+      "<leader>/",
+      LazyVim.pick("grep", { root = false }),
+      desc = "Grep (Root Dir)",
     },
   },
 }
