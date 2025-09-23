0eb816a9bc630b73e3351b9f07a592f1aef5c05e
Janne Vikstedt <vikstedt.janne@gmail.com>
2025-09-23 13:53:19 +0300

    Add opencode and update dependencies

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,6 +2,7 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
-  { "echasnovski/mini.pairs", enabled = false },
+  { "nvim-mini/mini.pairs", enabled = false },
   { "nvim-lualine/lualine.nvim", enabled = false },
+  { "MeanderingProgrammer/render-markdown.nvim", enabled = false },
 }
